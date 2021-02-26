df6e5df7ebcb52f14183c51cdfeafe339848ae5c
pauliusluksys <pauliusluksys2@gmail.com>
2021-02-26 14:28:35 +0200

    testing livewire

--- app/Http/Livewire/Ratings.php
+++ app/Http/Livewire/Ratings.php
@@ -8,9 +8,19 @@ class Ratings extends Component
 {
     public $book;
     public $message;
+    public $rateWindowOpen=false;
     public function render()
     {
 
         return view('livewire.ratings');
+    }
+    public function showRateWindow(){
+    if($this->rateWindowOpen===false) {
+        $this->rateWindowOpen=true;
+    }
+    else{
+        $this->rateWindowOpen=false;
+    }
+
     }
 }
--- resources/views/livewire/ratings.blade.php
+++ resources/views/livewire/ratings.blade.php
@@ -1,11 +1,19 @@
 <div>
     <form method="POST" action="">
 
-    <input wire:model="message" type="text">
+    <input wire:model.debounce.50ms="message" type="text">
 
         <button class="btn btn-danger">Submit</button>
         <div class="alert alert-success mt-2 d-none">Successfully saved</div>
+
     </form>
+
+
+    <button wire:click="showRateWindow">rate this book</button>
+    @if($rateWindowOpen===true)
+        Pageseeme
+    @else
+    @endif
     <h1>{{ $message }}</h1>
 
 </div>
